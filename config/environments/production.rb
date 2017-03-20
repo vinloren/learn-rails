e9022e7def3416b18d224652c9de720d590156c2
vinloren@gmail.com <vinloren@gmail.com>
2017-03-20 17:45:02 +0100

    set stmt port = 25

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,17 +59,15 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
 	address: "smtp.gmail.com",
-	port: 587,
+	port: 25,
 	domain: ENV["DOMAIN_NAME"],
 	authentication: "plain",
 	enable_starttls_auto: true,
 	user_name: ENV["GMAIL_USERNAME"],
 	password: ENV["GMAIL_PASSWORD"]
   }
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
   
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
