44a38f29e8f8a12509fc18d632bade2d05a8e922
vinloren@gmail.com <vinloren@gmail.com>
2017-03-20 18:56:48 +0100

    rest as before - suppressed defaul from

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-	default from: "do-not-reply@fakenews.com"
+	#default from: "do-not-reply@fakenews.com"
 	def contact_email(contact)
 		@contact = contact
 		mail(to: ENV["OWNER_EMAIL"], from: @contact.email, :subject => "Website Contact")
