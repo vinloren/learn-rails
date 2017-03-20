d66f20abc1c31e18fcff854d562d16bda4d2d6df
vinloren@gmail.com <vinloren@gmail.com>
2017-03-20 19:26:43 +0100

    set defaul from = OWNER_EMAIL

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-	#default from: "do-not-reply@fakenews.com"
+	default from: ENV["OWNER_EMAIL"]
 	def contact_email(contact)
 		@contact = contact
 		mail(to: ENV["OWNER_EMAIL"], from: @contact.email, :subject => "Website Contact")
